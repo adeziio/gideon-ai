b9bddda8a45613df635f5637663df2d028312549
Aden <adeziio@yahoo.com>
2023-08-02 13:59:41 -0400

    set up dtable diffusion endpoint

--- index.py
+++ index.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify,  request
 from flask_cors import CORS
 from dotenv import load_dotenv, find_dotenv
-from src import Auth
-from src import OpenAI
+from src.utils import Auth
+from src.models import OpenAI
+from src.models import StableDiffusion
 
 
 # Environment Variables
@@ -28,6 +29,18 @@ def gpt_3_5_turbo():
         input = body["input"]
         if (input):
             return jsonify(output=OpenAI.run("gpt-3.5-turbo", input))
+        return jsonify(error="Invalid parameter")
+    return jsonify(error="Unauthorized")
+
+
+@app.route("/stable-diffusion", methods=['POST'])
+def stable_diffusion():
+    key = request.headers.get("GIDEON_API_KEY")
+    if (Auth.checkAuth(key)):
+        body = request.json
+        input = body["input"]
+        if (input):
+            return jsonify(output=StableDiffusion.run(input))
         return jsonify(error="Invalid parameter")
     return jsonify(error="Unauthorized")
 
---
+++ src/models/StableDiffusion.py
@@ -0,0 +1,10 @@
+
+
+def run(input):
+    try:
+        input = input.lower()
+        response = ""
+
+        return response
+    except Exception as e:
+        return str(e)
