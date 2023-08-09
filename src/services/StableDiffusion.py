4fd00c398ee4bacedc2c731fb406fdf799a623bb
Aden <adeziio@yahoo.com>
2023-08-09 16:09:21 -0400

    testing

--- src/services/StableDiffusion.py
+++ src/services/StableDiffusion.py
@@ -1,22 +1,31 @@
 # import torch
-# from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionPipeline
 
 
-# def run(model, input):
-#     try:
-#         input = input.lower()
-#         response = ""
+def run(model, input):
+    try:
+        input = input.lower()
+        response = ""
 
-#         if (model == "aden"):
-#             pipe = StableDiffusionPipeline.from_single_file(
-#                 "src/models/aden.ckpt", torch_dtype=torch.float16,)
-#             pipe = pipe.to("cuda")
+        if (model == "aden"):
+            pipe = StableDiffusionPipeline.from_single_file(
+                "src/models/aden.ckpt",
+                # torch_dtype=torch.float16
+            )
+            pipe = pipe.to("cuda")
+            image = pipe(input).images[0]
+            # with torch.autocast('cuda'):
+            #     image = pipe(input).images[0]
 
-#             with torch.autocast('cuda'):
-#                 image = pipe(input).images[0]
+            image.save("temp/image.png")
+            import base64
 
-#             image.save("generated_image.png")
+            with open("temp/image.png", "rb") as image_file:
+                encoded_string = base64.b64encode(
+                    image_file.read()).decode('utf-8')
 
-#         return response
-#     except Exception as e:
-#         return str(e)
+            response = encoded_string
+
+        return response
+    except Exception as e:
+        return str(e)
