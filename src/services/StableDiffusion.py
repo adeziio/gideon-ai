ed7123c1bab621d9d3c8777108065fd1135b2b8a
Aden <adeziio@yahoo.com>
2023-08-09 16:22:24 -0400

    clean up

--- src/services/StableDiffusion.py
+++ src/services/StableDiffusion.py
@@ -8,22 +8,21 @@ def run(model, input):
         input = input.lower()
         response = ""
 
-        if (model == "aden"):
-            pipe = StableDiffusionPipeline.from_single_file(
-                "src/models/aden.ckpt",
-                torch_dtype=torch.float16
-            )
-            pipe = pipe.to("cuda")
+        pipe = StableDiffusionPipeline.from_single_file(
+            f"src/models/{model}.ckpt",
+            torch_dtype=torch.float16
+        )
+        pipe = pipe.to("cuda")
+        image = pipe(input).images[0]
+        with torch.autocast('cuda'):
             image = pipe(input).images[0]
-            with torch.autocast('cuda'):
-                image = pipe(input).images[0]
 
-            image.save("temp/image.png")
+        image.save("temp/image.png")
 
-            with open("temp/image.png", "rb") as image_file:
-                encoded_string = base64.b64encode(
-                    image_file.read()).decode('utf-8')
-            response = encoded_string
+        with open("temp/image.png", "rb") as image_file:
+            encoded_string = base64.b64encode(
+                image_file.read()).decode('utf-8')
+        response = encoded_string
         return response
     except Exception as e:
         return str(e)
